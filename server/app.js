1998bd919955f12fe5814f653b3f412938226dff
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-12-07 10:59:38 -0800

    added app.use graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql')
 //invoke the function to create our app
 const app = express();
+//function fires whenever a request for graphql is made
+app.use('graphql', graphHTTP({
+    //must pass a schema through here
+}))
 //tell the app to listen to port
 app.listen(4000, () => {
     //this is the callback function that will tell us when the port is working
