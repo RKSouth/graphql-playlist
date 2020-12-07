0ddc083351d68388591318caa5efd3d79a6cb5ba
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-12-07 09:52:15 -0800

    installed graphql and express-graphql

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+//invoke the function to create our app
 const app = express();
-
+//tell the app to listen to port
 app.listen(4000, () => {
+    //this is the callback function that will tell us when the port is working
     console.log('now listening for requests on port 4000');
 });
